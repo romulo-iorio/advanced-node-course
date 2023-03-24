798028d864530f144ad9c8e8197fc412daa07878
Rômulo Iorio <romuloiorio@hotmail.com>
2023-03-24 08:39:14 -0300

    feat: chaging test strategy to use jest mocks

--- tests/data/services/facebook-authentication.spec.ts
+++ tests/data/services/facebook-authentication.spec.ts
@@ -3,37 +3,27 @@ import type { LoadFacebookUserApi } from "@/data/contracts/apis";
 import { FacebookAuthenticationService } from "@/data/contracts/services";
 import { AuthenticationError } from "@/domain/errors";
 
-class LoadFacebookUserApiSpy implements LoadFacebookUserApi {
-  token?: string;
-  callsCount = 0;
-  result = undefined;
-
-  async loadUser(
-    params: LoadFacebookUserApi.Params
-  ): Promise<LoadFacebookUserApi.Result> {
-    this.token = params.token;
-    this.callsCount++;
-    return this.result;
-  }
-}
+const MOCK_PARAMS: LoadFacebookUserApi.Params = { token: "any_token" };
 
 describe("FacebookAuthenticationService", () => {
   it("Should call LoadFacebookUserApi with correct params", async () => {
-    const loadFacebookUserApi = new LoadFacebookUserApiSpy();
+    const loadFacebookUserApi = { loadUser: jest.fn() };
     const sut = new FacebookAuthenticationService(loadFacebookUserApi);
 
-    await sut.perform({ token: "any_token" });
+    await sut.perform(MOCK_PARAMS);
 
-    expect(loadFacebookUserApi.token).toBe("any_token");
-    expect(loadFacebookUserApi.callsCount).toBe(1);
+    expect(loadFacebookUserApi.loadUser).toHaveBeenCalledWith(MOCK_PARAMS);
+    expect(loadFacebookUserApi.loadUser).toHaveBeenCalledTimes(1);
   });
 
   it("Should return AuthenticationError when LoadFacebookUserApi returns undefined", async () => {
-    const loadFacebookUserApi = new LoadFacebookUserApiSpy();
-    loadFacebookUserApi.result = undefined;
+    const loadFacebookUserApi = { loadUser: jest.fn() };
+
+    loadFacebookUserApi.loadUser.mockResolvedValueOnce(undefined);
+
     const sut = new FacebookAuthenticationService(loadFacebookUserApi);
 
-    const authResult = await sut.perform({ token: "any_token" });
+    const authResult = await sut.perform(MOCK_PARAMS);
 
     expect(authResult).toEqual(new AuthenticationError());
   });
