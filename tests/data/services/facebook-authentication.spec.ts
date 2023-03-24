cac957dc07c4264dcb59c2f9accf08813c315b5e
Rômulo Iorio <romuloiorio@hotmail.com>
2023-03-24 08:47:37 -0300

    feat: simplifying code

--- tests/data/services/facebook-authentication.spec.ts
+++ tests/data/services/facebook-authentication.spec.ts
@@ -4,11 +4,10 @@ import type { LoadFacebookUserApi } from "@/data/contracts/apis";
 import { FacebookAuthenticationService } from "@/data/contracts/services";
 import { AuthenticationError } from "@/domain/errors";
 
-const MOCK_PARAMS: LoadFacebookUserApi.Params = { token: "any_token" };
-
 describe("FacebookAuthenticationService", () => {
   let loadFacebookUserApi: MockProxy<LoadFacebookUserApi>;
   let sut: FacebookAuthenticationService;
+  const token = "any_token";
 
   beforeEach(() => {
     loadFacebookUserApi = mock<LoadFacebookUserApi>();
@@ -16,16 +15,16 @@ describe("FacebookAuthenticationService", () => {
   });
 
   it("Should call LoadFacebookUserApi with correct params", async () => {
-    await sut.perform(MOCK_PARAMS);
+    await sut.perform({ token });
 
-    expect(loadFacebookUserApi.loadUser).toHaveBeenCalledWith(MOCK_PARAMS);
+    expect(loadFacebookUserApi.loadUser).toHaveBeenCalledWith({ token });
     expect(loadFacebookUserApi.loadUser).toHaveBeenCalledTimes(1);
   });
 
   it("Should return AuthenticationError when LoadFacebookUserApi returns undefined", async () => {
     loadFacebookUserApi.loadUser.mockResolvedValueOnce(undefined);
 
-    const authResult = await sut.perform(MOCK_PARAMS);
+    const authResult = await sut.perform({ token });
 
     expect(authResult).toEqual(new AuthenticationError());
   });
