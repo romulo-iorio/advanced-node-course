cf386fdfb675c3b109df1330a31cbc76200af0a4
Rômulo Iorio <romuloiorio@hotmail.com>
2023-04-14 08:56:55 -0300

    feat: isolating SUT creation

--- tests/infra/http/axios-client.spec.ts
+++ tests/infra/http/axios-client.spec.ts
@@ -11,17 +11,28 @@ class AxiosHttpClient {
 }
 
 describe("AxiosHttpClient", () => {
+  let fakeAxios: jest.Mocked<typeof axios>;
+  let sut: AxiosHttpClient;
+
+  let params: object;
+  let url: string;
+
+  beforeAll(() => {
+    fakeAxios = axios as jest.Mocked<typeof axios>;
+
+    params = { any: "any" };
+    url = "any_url";
+  });
+
+  beforeEach(() => {
+    sut = new AxiosHttpClient();
+  });
+
   describe("get", () => {
     it("should call get with correct params", async () => {
-      const fakeAxios = axios as jest.Mocked<typeof axios>;
+      await sut.get({ url, params });
 
-      const sut = new AxiosHttpClient();
-
-      await sut.get({ url: "any_url", params: { any: "any" } });
-
-      expect(fakeAxios.get).toHaveBeenCalledWith("any_url", {
-        params: { any: "any" },
-      });
+      expect(fakeAxios.get).toHaveBeenCalledWith(url, { params });
 
       expect(fakeAxios.get).toHaveBeenCalledTimes(1);
     });
