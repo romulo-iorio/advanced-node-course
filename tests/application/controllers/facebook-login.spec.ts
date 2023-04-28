b3dd76ee1cc069804d6864868b8c1e0f82d6d46f
Rômulo Iorio <romuloiorio@hotmail.com>
2023-04-28 13:38:05 -0300

    feat: ensuring FacebookLoginController calls FacebooAuth with correct params

--- tests/application/controllers/facebook-login.spec.ts
+++ tests/application/controllers/facebook-login.spec.ts
@@ -1,7 +1,16 @@
+import type { FacebookAuthentication } from "@/domain/features";
+import { mock } from "jest-mock-extended";
+
 type HttpResponse = { statusCode: number; data: any };
 
 class FacebookLoginController {
+  constructor(
+    private readonly facebookAuthentication: FacebookAuthentication
+  ) {}
+
   async handle(httpRequest: any): Promise<HttpResponse> {
+    await this.facebookAuthentication.perform({ token: httpRequest.token });
+
     return {
       statusCode: 400,
       data: new Error("The field token is required"),
@@ -11,7 +20,8 @@ class FacebookLoginController {
 
 describe("FacebookLoginController", () => {
   it("should return 400 if token is empty", async () => {
-    const sut = new FacebookLoginController();
+    const facebookAuth = mock<FacebookAuthentication>();
+    const sut = new FacebookLoginController(facebookAuth);
 
     const httpResponse = await sut.handle({ token: "" });
 
@@ -22,7 +32,8 @@ describe("FacebookLoginController", () => {
   });
 
   it("should return 400 if token is null", async () => {
-    const sut = new FacebookLoginController();
+    const facebookAuth = mock<FacebookAuthentication>();
+    const sut = new FacebookLoginController(facebookAuth);
 
     const httpResponse = await sut.handle({ token: null });
 
@@ -33,7 +44,8 @@ describe("FacebookLoginController", () => {
   });
 
   it("should return 400 if token is undefined", async () => {
-    const sut = new FacebookLoginController();
+    const facebookAuth = mock<FacebookAuthentication>();
+    const sut = new FacebookLoginController(facebookAuth);
 
     const httpResponse = await sut.handle({ token: undefined });
 
@@ -41,5 +53,15 @@ describe("FacebookLoginController", () => {
       statusCode: 400,
       data: new Error("The field token is required"),
     });
+  });
+
+  it("should call FacebookAuthentication with correct params", async () => {
+    const facebookAuth = mock<FacebookAuthentication>();
+    const sut = new FacebookLoginController(facebookAuth);
+
+    await sut.handle({ token: "any_token" });
+
+    expect(facebookAuth.perform).toHaveBeenCalledWith({ token: "any_token" });
+    expect(facebookAuth.perform).toHaveBeenCalledTimes(1);
   });
 });
