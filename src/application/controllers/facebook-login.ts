30fc620e090cc420cb5d4ecf20f7f5cccc8658b2
Rômulo Iorio <romuloiorio@hotmail.com>
2023-05-05 08:49:11 -0300

    feat applying ok helper

--- src/application/controllers/facebook-login.ts
+++ src/application/controllers/facebook-login.ts
@@ -5,6 +5,7 @@ import {
   unauthorized,
   serverError,
   badRequest,
+  ok,
 } from "@/application/helpers";
 import { RequiredFieldError } from "@/application/errors";
 import { AccessToken } from "@/domain/models";
@@ -15,21 +16,16 @@ export class FacebookLoginController {
   ) {}
 
   async handle(httpRequest: any): Promise<HttpResponse> {
+    const { token } = httpRequest;
+
     try {
-      if (httpRequest.token === "" || httpRequest.token == null) {
+      if (token === "" || token == null)
         return badRequest(new RequiredFieldError("token"));
-      }
 
-      const accessToken = await this.facebookAuthentication.perform({
-        token: httpRequest.token,
-      });
+      const accessToken = await this.facebookAuthentication.perform({ token });
 
-      if (accessToken instanceof AccessToken) {
-        return {
-          statusCode: 200,
-          data: { accessToken: accessToken.value },
-        };
-      }
+      if (accessToken instanceof AccessToken)
+        return ok({ accessToken: accessToken.value });
 
       return unauthorized();
     } catch (err) {
