19d74f808301bf630e2556b12bca8b10a8c43616
Rômulo Iorio <romuloiorio@hotmail.com>
2023-05-05 08:39:17 -0300

    feat applying badRequest helper

--- src/application/controllers/facebook-login.ts
+++ src/application/controllers/facebook-login.ts
@@ -1,7 +1,6 @@
+import { RequiredFieldError, ServerError } from "@/application/errors";
+import { type HttpResponse, badRequest } from "@/application/helpers";
 import type { FacebookAuthentication } from "@/domain/features";
-import type { HttpResponse } from "@/application/helpers";
-
-import { ServerError } from "@/application/errors";
 import { AccessToken } from "@/domain/models";
 
 export class FacebookLoginController {
@@ -12,10 +11,7 @@ export class FacebookLoginController {
   async handle(httpRequest: any): Promise<HttpResponse> {
     try {
       if (httpRequest.token === "" || httpRequest.token == null) {
-        return {
-          statusCode: 400,
-          data: new Error("The field token is required"),
-        };
+        return badRequest(new RequiredFieldError("token"));
       }
 
       const result = await this.facebookAuthentication.perform({
--- tests/application/controllers/facebook-login.spec.ts
+++ tests/application/controllers/facebook-login.spec.ts
@@ -1,9 +1,9 @@
+import type { FacebookAuthentication } from "@/domain/features";
 import { type MockProxy, mock } from "jest-mock-extended";
 
-import type { FacebookAuthentication } from "@/domain/features";
+import { RequiredFieldError, ServerError } from "@/application/errors";
 import { FacebookLoginController } from "@/application/controllers";
 import { AuthenticationError } from "@/domain/errors";
-import { ServerError } from "@/application/errors";
 import { AccessToken } from "@/domain/models";
 
 describe("FacebookLoginController", () => {
@@ -24,7 +24,7 @@ describe("FacebookLoginController", () => {
 
     expect(httpResponse).toEqual({
       statusCode: 400,
-      data: new Error("The field token is required"),
+      data: new RequiredFieldError("token"),
     });
   });
 
@@ -33,7 +33,7 @@ describe("FacebookLoginController", () => {
 
     expect(httpResponse).toEqual({
       statusCode: 400,
-      data: new Error("The field token is required"),
+      data: new RequiredFieldError("token"),
     });
   });
 
@@ -42,7 +42,7 @@ describe("FacebookLoginController", () => {
 
     expect(httpResponse).toEqual({
       statusCode: 400,
-      data: new Error("The field token is required"),
+      data: new RequiredFieldError("token"),
     });
   });
 
