368f23ec986a9161f6f11fe88a351e22884e508c
Rômulo Iorio <romuloiorio@hotmail.com>
2023-05-12 08:55:05 -0300

    feat: moving production code to their own files

--- tests/application/validation/composite.spec.ts
+++ tests/application/validation/composite.spec.ts
@@ -1,19 +1,6 @@
 import { type MockProxy, mock } from "jest-mock-extended";
 
-interface Validator {
-  validate: () => Error | undefined;
-}
-
-class ValidationComposite implements Validator {
-  constructor(private readonly validators: Validator[]) {}
-
-  validate(): Error | undefined {
-    for (const validator of this.validators) {
-      const error = validator.validate();
-      if (error !== undefined) return error;
-    }
-  }
-}
+import { type Validator, ValidationComposite } from "@/application/validation";
 
 describe("ValidationComposite", () => {
   let sut: ValidationComposite;
