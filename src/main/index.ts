108ff24d317f1603f775f24931138aaea99ad3d9
Rômulo Iorio <romuloiorio@hotmail.com>
2023-05-26 09:55:34 -0300

    feat: creating minimal express app

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,2 +1,25 @@
+import "./config/module-alias";
+
 import "reflect-metadata";
 import "dotenv/config";
+
+import express, { Router, json } from "express";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+app.use(json());
+app.use((_, res, next) => {
+  res.type("json");
+  next();
+});
+
+const router = Router();
+
+router.post("/api/login/facebook", (_, res) => {
+  res.send({ data: "any_data" });
+});
+
+app.use(router);
+
+app.listen(8080, () => console.log("Server running on http://localhost:8080"));
