8711e8cf45b3017e22265878f277fced1d803a04
Rômulo Iorio <romuloiorio@hotmail.com>
2023-05-26 09:41:50 -0300

    refactor: isolating SUT creation

--- tests/external/facebook-api.test.ts
+++ tests/external/facebook-api.test.ts
@@ -7,10 +7,15 @@ const { token, user } = testData;
 const { id: facebookId, email, name } = user;
 
 describe("Facebook Api Integration Tests", () => {
-  it("should return a Facebook User if token is valid", async () => {
-    const axiosClient = new AxiosHttpClient();
-    const sut = new FacebookApi(axiosClient, clientId, clientSecret);
+  let axiosClient: AxiosHttpClient;
+  let sut: FacebookApi;
 
+  beforeEach(() => {
+    axiosClient = new AxiosHttpClient();
+    sut = new FacebookApi(axiosClient, clientId, clientSecret);
+  });
+
+  it("should return a Facebook User if token is valid", async () => {
     const fbUser = await sut.loadUser({ token });
 
     expect(fbUser).toEqual({ facebookId, email, name });
