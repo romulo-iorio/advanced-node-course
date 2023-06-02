a54f8205b16c067642a28c45eb7b02b184b6b5fe
Rômulo Iorio <romuloiorio@hotmail.com>
2023-06-02 08:41:37 -0300

    feat: ensuring ExpressRouter responds with 400 and correct error if controller return error

--- tests/infra/http/express-router.spec.ts
+++ tests/infra/http/express-router.spec.ts
@@ -11,7 +11,11 @@ class ExpressRouter {
   async adapt(req: Request, res: Response): Promise<void> {
     const httpResponse = await this.controller.handle({ ...req.body });
 
-    res.status(200).json(httpResponse.data);
+    if (httpResponse.statusCode === 200)
+      res.status(200).json(httpResponse.data);
+
+    if (httpResponse.statusCode === 400)
+      res.status(400).json({ error: httpResponse.data.message });
   }
 }
 
@@ -56,6 +60,21 @@ describe("ExpressRouter", () => {
     expect(res.status).toHaveBeenCalledTimes(1);
 
     expect(res.json).toHaveBeenCalledWith({ ...data });
+    expect(res.json).toHaveBeenCalledTimes(1);
+  });
+
+  it("should respond with 400 and valid error", async () => {
+    controller.handle.mockResolvedValueOnce({
+      statusCode: 400,
+      data: new Error("any_error"),
+    });
+
+    await sut.adapt(req, res);
+
+    expect(res.status).toHaveBeenCalledWith(400);
+    expect(res.status).toHaveBeenCalledTimes(1);
+
+    expect(res.json).toHaveBeenCalledWith({ error: "any_error" });
     expect(res.json).toHaveBeenCalledTimes(1);
   });
 });
