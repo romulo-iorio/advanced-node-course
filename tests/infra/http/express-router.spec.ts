61e6f682dc70278da6f8caa2544e92db275c9ce8
Rômulo Iorio <romuloiorio@hotmail.com>
2023-06-02 08:28:53 -0300

    feat: ensuring ExpressRouter calls handle with empty request

--- tests/infra/http/express-router.spec.ts
+++ tests/infra/http/express-router.spec.ts
@@ -24,4 +24,16 @@ describe("ExpressRouter", () => {
 
     expect(controller.handle).toHaveBeenCalledWith({ any: "any" });
   });
+
+  it("should call handle with empty request", async () => {
+    const req = getMockReq();
+    const { res } = getMockRes();
+
+    const controller = mock<Controller>();
+    const sut = new ExpressRouter(controller);
+
+    await sut.adapt(req, res);
+
+    expect(controller.handle).toHaveBeenCalledWith({});
+  });
 });
